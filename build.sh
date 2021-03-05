90a896af13ec1e212ac637bb5dffbb2eeeb82c5b
Carey J. Mataka <cmataka@chii.ml>
2021-03-05 12:58:58 +0100

    added some mutables markers ... :)

--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
 # 
 
 # note these are commited Qms so they are immutable ones i.e. not current one
-qmgit=Qma2hJscEDyVCvPx7odLvdGaajMuJdgkvBCPuCcD8Xc3qq
-qmrepo=QmTpXxAR6M2rWAzYibCT9nE2sbNmjGvhigrk8z9Cyu4n4h
+qmgit=QmYAWBZBVvY9jHvk4v5FvEinLXGHFEXMVUd1FJQSKAtXD9
+qmrepo=QmPf7fiocTkqEoznyDq8Jmpzj1V7BXLNtAiQAgMqTgibJG
 qm=QmXBzhWQ3URLbYUi49JPQC5PXBJBvkJ1WyeiyD6wVyNgkH
 
-curl -m 10 -sL https://gateway.ipfs.io/api/v0/ls?arg=$qmrepo
+tic=$(date +%s)
+curl -m 3 -sL https://gateway.ipfs.io/api/v0/ls?arg=$qmrepo &
 
 git cat-file -p master^{tree}
 git rev-parse HEAD:
 git log -1
 
-curl -iL https://ipfs.blockring™.ml/ipfs/$qmgit/info/refs
+curl -iL https://ipfs.blockring™.ml/ipfs/$qmgit/info/refs &
 git pull --no-rebase https://ipfs.blockring™.ml/ipfs/$qmrepo/default.site.git
 
 git cat-file -p master^{tree}
 git rev-parse HEAD:
 git log -1
+sed -s "s/tic: .*/tic: $tic/" mutables.tmpl > mutables.js
+
+git status
 
 echo done.
-
 exit $?
 true;

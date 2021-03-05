99288a53c45a3f660b2b0a20b2db251c7a4e185c
Geneva E. Mcelmarry <gmcelmarry@clockchain.tk>
2021-03-05 11:55:55 +0100

    added initial qms for qmgit and qmrepo

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # 
 
 # note these are commited Qms so they are immutable ones i.e. not current one
-qmrepo=QmSZDpkJBEpUvBmnWauY4gcbcGrB8Ctzr8gEjTNLTRoYVZ
-qmgit=QmQXEHYorfHFZxttZK942bFWxjGdM3Ej3H48v7wPpW6S45
-qm=QmaXhAtgJGupFx5jSsBhWhmdfnULbyB2uLDDUZP7d3hZnh
+qmrepo=Qma2hJscEDyVCvPx7odLvdGaajMuJdgkvBCPuCcD8Xc3qq
+qmgit=QmTpXxAR6M2rWAzYibCT9nE2sbNmjGvhigrk8z9Cyu4n4h
+qm=QmXBzhWQ3URLbYUi49JPQC5PXBJBvkJ1WyeiyD6wVyNgkH
 
 curl -sL https://ipfs.blockring™.ml/api/v0/ls?arg=$qmgit
 

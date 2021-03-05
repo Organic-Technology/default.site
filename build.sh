eb6cd9cb4b32f3e26f323b684a66c76f01f6b2df
Henry S. Honhart <hhonhart@keysm.ml>
2021-03-05 11:21:08 +0100

    use qmgit instead of qmrepo

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 # 
 
+# note these are commited Qms so they are immutable ones i.e. not current one
+qmrepo=QmSZDpkJBEpUvBmnWauY4gcbcGrB8Ctzr8gEjTNLTRoYVZ
 qmgit=QmQXEHYorfHFZxttZK942bFWxjGdM3Ej3H48v7wPpW6S45
-qm=~
+qm=QmaXhAtgJGupFx5jSsBhWhmdfnULbyB2uLDDUZP7d3hZnh
 
 curl -sL https://ipfs.blockring™.ml/api/v0/ls?arg=$qmgit
 
@@ -9,8 +11,8 @@ git cat-file -p master^{tree}
 git rev-parse HEAD:
 git log -1
 
-curl -iL https://ipfs.blockring™.ml/ipfs/$qmgit/default.site.git/info/refs
-git pull --no-rebase https://ipfs.blockring™.ml/ipfs/$qmgit/default.site.git
+curl -iL https://ipfs.blockring™.ml/ipfs/$qmrepo/default.site.git/info/refs
+git pull --no-rebase https://ipfs.blockring™.ml/ipfs/$qmrepo/default.site.git
 
 git cat-file -p master^{tree}
 git rev-parse HEAD:

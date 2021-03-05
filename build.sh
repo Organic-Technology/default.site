2fb277e02cdfc8d97b338fa8446382a3e0d78ee1
Letha J. Serve <lserve@ydentity.ml>
2021-03-05 10:47:41 +0100

    added sed to update Qms !

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 # 
 
-qm=QmQXEHYorfHFZxttZK942bFWxjGdM3Ej3H48v7wPpW6S45
+qmgit=QmQXEHYorfHFZxttZK942bFWxjGdM3Ej3H48v7wPpW6S45
+qm=~
+
+curl -sL https://ipfs.blockring™.ml/api/v0/ls?arg=$qmgit
 
 git cat-file -p master^{tree}
 git rev-parse HEAD:
 git log -1
 
-curl -iL https://ipfs.blockring™.ml/ipfs/$qm/default.site.git/info/refs
-git pull --no-rebase https://ipfs.blockring™.ml/ipfs/$qm/default.site.git
+curl -iL https://ipfs.blockring™.ml/ipfs/$qmgit/default.site.git/info/refs
+git pull --no-rebase https://ipfs.blockring™.ml/ipfs/$qmgit/default.site.git
 
 git cat-file -p master^{tree}
 git rev-parse HEAD:

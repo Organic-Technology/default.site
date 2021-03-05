b7a89e70a424bfd07bd01aa5f3b3a81427cfdadc
Adele M. Gnerre <agnerre@blockringtm.gq>
2021-03-05 12:10:25 +0100

    switched qmrepo and qmgit again !

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 # 
 
 # note these are commited Qms so they are immutable ones i.e. not current one
-qmrepo=Qma2hJscEDyVCvPx7odLvdGaajMuJdgkvBCPuCcD8Xc3qq
-qmgit=QmTpXxAR6M2rWAzYibCT9nE2sbNmjGvhigrk8z9Cyu4n4h
+qmgit=Qma2hJscEDyVCvPx7odLvdGaajMuJdgkvBCPuCcD8Xc3qq
+qmrepo=QmTpXxAR6M2rWAzYibCT9nE2sbNmjGvhigrk8z9Cyu4n4h
 qm=QmXBzhWQ3URLbYUi49JPQC5PXBJBvkJ1WyeiyD6wVyNgkH
 
-curl -sL https://ipfs.blockring™.ml/api/v0/ls?arg=$qmgit
+curl -m 10 -sL https://gateway.ipfs.io/api/v0/ls?arg=$qmrepo
 
 git cat-file -p master^{tree}
 git rev-parse HEAD:
 git log -1
 
-curl -iL https://ipfs.blockring™.ml/ipfs/$qmrepo/default.site.git/info/refs
+curl -iL https://ipfs.blockring™.ml/ipfs/$qmgit/info/refs
 git pull --no-rebase https://ipfs.blockring™.ml/ipfs/$qmrepo/default.site.git
 
 git cat-file -p master^{tree}
 git rev-parse HEAD:
 git log -1
 
+echo done.
 
+exit $?
+true;

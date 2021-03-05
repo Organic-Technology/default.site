a7c7f7e0b50286577de8d61e735db4a3fbb60708
Luann K. Rapaport <lrapaport@happycoin.cf>
2021-03-05 14:01:01 +0100

    new qmgit !

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # 
 
 # note these are commited Qms so they are immutable ones i.e. not current one
-qmgit=QmXhiZfDzhcVCwvwq9DFBz5KaxrSxdXemJSApiZj5M54NN
-qmrepo=QmV5mbp6eEvdaZTJqFAytFLobqadX6tLVNrXY3m4fXvxHr
-qm=QmR8TiBM8L86PXs3bkUMUrBfYeNUFoTXFD5aRNwn6Ho4Ur
+qmgit=QmZKfXZc3rwVrAnqybccQtqpY8gjzfUa7puWiJZYRaxpvv
+qmrepo=QmRScW4ZhiMPYJsLZevNHHC7HmdY3fZFmMPoV1oZRxHz2V
+qm=Qmc223eTNPHJa7TfeY7R7ArrBFsbnqqZLdehRbL2SXhH5D
 
 tic=$(date +%s)
 curl -m 3 -sL https://gateway.ipfs.io/api/v0/ls?arg=$qmrepo &

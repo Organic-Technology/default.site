197f3aaac294231901549563e343e1c8f85f6f10
Letha J. Serve <lserve@ydentity.ml>
2021-03-05 10:26:15 +0100

    added a build file for CI/CD

---
+++ build.sh
@@ -0,0 +1,16 @@
+# 
+
+qm=QmQXEHYorfHFZxttZK942bFWxjGdM3Ej3H48v7wPpW6S45
+
+git cat-file -p master^{tree}
+git rev-parse HEAD:
+git log -1
+
+curl -iL https://ipfs.blockring™.ml/ipfs/$qm/default.site.git/info/refs
+git pull --no-rebase https://ipfs.blockring™.ml/ipfs/$qm/default.site.git
+
+git cat-file -p master^{tree}
+git rev-parse HEAD:
+git log -1
+
+
